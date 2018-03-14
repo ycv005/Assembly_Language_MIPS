9d4aebd3f8553b1095aa65267f888a10f043bc83
Yash Chandra Verma <ycverma005@gmail.com>
2018-03-14 10:43:12 +0530

    Add files via upload

---
+++ Save_t_Registers_Stack.asm
@@ -0,0 +1,29 @@
+.data
+    newline: .asciiz "\n"
+.text
+    main:               #main is our caller function
+    addi $t0, 20
+    
+    jal addnum
+
+    li $v0, 4
+    la $a0, newline
+    syscall
+    
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    li $v0, 10
+    syscall
+
+    addnum:             #addnum is our calle function
+        li $v0, 1
+        add $a0, $t0, 10
+        syscall
+        jr $ra
+
+#----------code explanation------------#
+#Caller function saves the value of temporary registers, argument registers, & value registers(function return value).
+#Here, we are trying to modify the $t registers and it get modified for a instance as the caller save the value of the $t register and caller gives permission to the calle function to do so.
+
