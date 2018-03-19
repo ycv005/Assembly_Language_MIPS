adc73e29b451ddcc7b8e2921272f6521554c93c6
Yash Chandra Verma <ycverma005@gmail.com>
2018-03-20 00:20:41 +0530

    Update Recursion_Factorial.asm

--- Recursion_Factorial.asm
+++ Recursion_Factorial.asm
@@ -1,14 +1,13 @@
 .data
-
 .text
 main:
     li $v0, 5
     syscall
 
-    move $t0, $v0
     #calling the factorial value
     #so we need the user input value as the argument for the factorial function
-    move $a0, $t0     #$a0 will be acting as the argument for the given function
+
+    move $a0, $v0     #$a0 will be acting as the argument for the given function
     jal findfact
     #storing the result
     move $s0, $v0
@@ -21,25 +20,25 @@ main:
     li $v0, 10
     syscall
 
-    findfact:
+    findfact:                   #create space in stack about 8 bytes to store the address present in the $ra and argument $a0 which will be moved to $s0
         sub $sp, $sp, 8
         sw $ra, 0($sp)
         sw $s0, 4($sp)
 
         #base case or when to stop the recursion
 
-        li $v0, 1
-        beq $a0, 0, factdone
+        li $v0, 1                   #before we jump to the factdone, assigning the value of the $v0 as 1.
+        beq $a0, 0, factdone        #if the argument $a0 value is 0, then program will be jumped to the factdone
 
-        move $s0, $a0
-        sub $a0, $a0, 1
+        move $s0, $a0               #argument will be used in every fucntion call, so we are storing its value to a other register
+        sub $a0, $a0, 1             #$a0 = $a0 - 1
 
-        jal findfact
+        jal findfact                
 
-        mul $v0, $v0, $s0
+        mul $v0, $v0, $s0           #$v0 = $v0 x $s0
 
         factdone:
             lw $ra, 0($sp)
             lw $s0, 4($sp)
             add $sp, $sp, 8
-            jr $ra
+            jr $ra              #it will jump to the instruction next to the jal findfact that is mul and then factdone wil again come in role.
