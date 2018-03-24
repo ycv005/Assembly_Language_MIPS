65427a869ee2c2469b875a45f2f38e6382d57e1d
Yash Chandra Verma <ycverma005@gmail.com>
2018-03-24 17:40:02 +0530

    Update Get user input_String.asm

--- Get user input_String.asm
+++ Get user input_String.asm
@@ -8,16 +8,18 @@
     syscall
 
     li $v0, 8       #8 is the system call code for the getting string as input
-    la $a0, userinput       #$a0 stores the address where input has to given
+    la $a0, userinput       #Always need to pass address of the variable to $a0 (stores the address where input has to given)
     la $a1, 30              #$a1 is used to tell my maximum length of the input is 30
     syscall
 
+    move $t0, $a0
+    
     li $v0, 4
     la $a0, msg
     syscall
 
     li $v0, 4
-    la $a0, userinput
+    move $a0, $t0   #we are not using here la instruction bcoz we are not loading the memory.
     syscall
 
     li $v0, 10
